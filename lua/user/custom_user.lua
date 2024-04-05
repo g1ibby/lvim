d14a94afc3ec96aae2b894ad6bcb795b629a4e4e
g1ibby <me@appci.xyz>
2024-04-05 22:15:20 +0700

    custom configs

--- lua/user/custom_user.lua
+++ lua/user/custom_user.lua
@@ -61,7 +61,7 @@ M.config = function()
   lvim.builtin.harpoon.active = true
   lvim.builtin.symbols_usage.active = true
   lvim.builtin.tag_provider = "outline"
-  lvim.builtin.bigfile.active = false
+  lvim.builtin.bigfile.active = true
   -- require("lvim.lsp.manager").setup("prosemd_lsp", {})
 end
 
