b25b944fea098c43b5b7518e2468a1fa8515d5fc
g1ibby <me@appci.xyz>
2024-05-01 19:48:48 +0700

    disabled auto change root directory

--- lua/user/custom_user.lua
+++ lua/user/custom_user.lua
@@ -62,6 +62,7 @@ M.config = function()
   lvim.builtin.symbols_usage.active = true
   lvim.builtin.tag_provider = "outline"
   lvim.builtin.bigfile.active = true
+  lvim.builtin.project.manual_mode = true
   -- require("lvim.lsp.manager").setup("prosemd_lsp", {})
 end
 
